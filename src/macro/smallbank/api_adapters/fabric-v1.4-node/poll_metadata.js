cdb914b4feab174ce3b0387d1586e766bd9ebcb5
David <d.satomi@outlook.de>
2020-07-20 18:34:39 +0200

    poll_metadata for smallbank

---
+++ src/macro/smallbank/api_adapters/fabric-v1.4-node/poll_metadata.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const ccUtil = require("./ccutil.js")
+
+if (process.argv.length < 3) {
+    console.log("Invalid parameter...");
+    console.log("Should be 'node query_blk_height.js <ordererAddr> <peerAddr> <blk number>'");
+    process.exit(1);
+}
+
+var ordererAddr = "grpc://" + process.argv[2];
+var peerAddr = "grpc://" + process.argv[3];
+var blockNum =  parseInt(process.argv[4]);
+
+var channel;
+var result = new Object;
+
+Promise.resolve().then(()=>{
+    return ccUtil.createChannelAndClient(peerAddr, ordererAddr);
+}).then((result)=>{
+    channel = result.channel;
+    return channel.queryBlock(blockNum);
+}).then((block)=>{
+    result["status"] = [];
+    //result["txns"] = [];
+    var txs_num = block.data.data.length;
+    result["txs_num"] = [txs_num];
+
+    let tx_filters = block.metadata.metadata[2]
+    for (var index = 0; index < block.data.data.length; index++) {
+        //var channel_header = block.data.data[index].payload.header.channel_header;
+        result["status"].push(tx_filters[index])
+        //result["txns"].push(channel_header.tx_id)
+    }
+    
+    console.log(result)
+}).catch((err)=>{
+    result["status"] = "err";
+    result["msg"] = err.message;
+    console.log(result)
+});
